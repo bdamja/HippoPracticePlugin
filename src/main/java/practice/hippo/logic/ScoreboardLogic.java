e8f2b9a8e7bf904010961c2303fb236d89db6850
bdamja <iceicebabyalter@gmail.com>
2023-02-26 15:35:24 -0700

    scoreboard design

--- src/main/java/practice/hippo/logic/ScoreboardLogic.java
+++ src/main/java/practice/hippo/logic/ScoreboardLogic.java
@@ -8,6 +8,14 @@ import org.bukkit.scoreboard.*;
 public class ScoreboardLogic {
 
     private final ScoreboardManager manager;
+    private static final String IP = ChatColor.DARK_GREEN + "someserver.net";
+    private static final String MAP_LABEL = "" + ChatColor.GREEN + ChatColor.BOLD + "Map" + ChatColor.GRAY + ChatColor.BOLD + ":";
+    private static final String PB_LABEL = "" + ChatColor.YELLOW + ChatColor.BOLD + "Personal Best" + ChatColor.GRAY + ChatColor.BOLD + ":";
+    private static final String TIME_LABEL = "" + ChatColor.AQUA + ChatColor.BOLD + "Time" + ChatColor.GRAY + ChatColor.BOLD + ":";
+    private static final String DEFAULT_TIME = "" + ChatColor.GRAY + "-.--- ";
+    private static final String DEFAULT_PB = "" + ChatColor.GRAY + "-.---";
+    private static final String DEFAULT_MAP_NAME = "" + ChatColor.GRAY + ChatColor.BOLD + "----";
+    private static final String DARK_GRAY_LINE = "" + ChatColor.DARK_GRAY + ChatColor.STRIKETHROUGH + "-------------------";
 
     public ScoreboardLogic() {
         manager = Bukkit.getScoreboardManager();
@@ -19,12 +27,29 @@ public class ScoreboardLogic {
         objective.setDisplayName("" + ChatColor.DARK_GREEN + ChatColor.BOLD + "Hippo Practice");
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-        Team blank1 = board.registerNewTeam("");
-        blank1.addEntry("");
-        blank1.setSuffix("");
-        blank1.setPrefix("");
-        objective.getScore("").setScore(1);
+        registerTeam("lineA", DARK_GRAY_LINE, "", "", 11, board, objective);
+
+        registerTeam("mapLabel", MAP_LABEL, "", "", 10, board, objective);
+        registerTeam("mapName", DEFAULT_MAP_NAME, "", "", 9, board, objective);
+        registerTeam("pbLabel", PB_LABEL, "", "", 7, board, objective);
+        registerTeam("pbName", DEFAULT_PB, "", "", 6, board, objective);
+        registerTeam("timeLabel", TIME_LABEL, "", "", 4, board, objective);
+        registerTeam("timeName", DEFAULT_TIME, "", "", 3, board, objective);
+        registerTeam("lineB", DARK_GRAY_LINE + " ", "", "", 1, board, objective);
+        registerTeam("ip", IP, "", "", 0, board, objective);
+
+        registerTeam("blank2", " ", "", "", 2, board, objective);
+        registerTeam("blank5", "  ", "", "", 5, board, objective);
+        registerTeam("blank8", "   ", "", "", 8, board, objective);
 
         player.setScoreboard(board);
+    }
+
+    private void registerTeam(String teamID, String teamName, String prefix, String suffix, int score, Scoreboard board, Objective objective) {
+        Team newTeam = board.registerNewTeam(teamID);
+        newTeam.addEntry(teamName);
+        newTeam.setPrefix(prefix);
+        newTeam.setSuffix(suffix);
+        objective.getScore(teamName).setScore(score);
     }
 }
